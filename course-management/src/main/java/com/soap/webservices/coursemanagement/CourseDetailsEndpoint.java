b4432f17f8212e586cbfd9fc2f1085f78b0f4b70
Nick Mazzotta <nickamazzotta@gmail.com>
2018-02-11 17:34:09 -0500

    sources generated correctly into source folder

--- course-management/src/main/java/com/soap/webservices/coursemanagement/CourseDetailsEndpoint.java
+++ course-management/src/main/java/com/soap/webservices/coursemanagement/CourseDetailsEndpoint.java
@@ -1,5 +1,8 @@
 package com.soap.webservices.coursemanagement;
 
+import com.in28minutes.courses.GetCourseDetailsRequest;
+import com.in28minutes.courses.GetCourseDetailsResponse;
+
 public class CourseDetailsEndpoint {
 
 	public GetCourseDetailsResponse processCourseDetailsRequest(GetCourseDetailsRequest request) {
