a90a8527dd00fc153d9e0e0339d1d36d93632e23
Nick Mazzotta <nickamazzotta@gmail.com>
2018-02-13 20:42:18 -0500

    updating endpoints

--- course-management/src/main/java/com/soap/webservices/coursemanagement/soap/CourseDetailsEndpoint.java
+++ course-management/src/main/java/com/soap/webservices/coursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,5 +1,6 @@
 package com.soap.webservices.coursemanagement.soap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -8,14 +9,21 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import com.in28minutes.courses.CourseDetails;
 import com.in28minutes.courses.GetCourseDetailsRequest;
 import com.in28minutes.courses.GetCourseDetailsResponse;
+import com.soap.webservices.coursemanagement.soap.bean.Course;
+import com.soap.webservices.coursemanagement.soap.service.CourseDetailsService;
 
 @Endpoint
 public class CourseDetailsEndpoint {
+	
+	@Autowired
+	CourseDetailsService service;
 
 	@PayloadRoot(namespace="http://in28minutes.com/courses", localPart="GetCourseDetailsRequest")
 	@ResponsePayload
 	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
 		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		
+		Course course = service.findById(request.getId());
 		
 		CourseDetails courseDetails = new CourseDetails();
 		courseDetails.setId(request.getId());
