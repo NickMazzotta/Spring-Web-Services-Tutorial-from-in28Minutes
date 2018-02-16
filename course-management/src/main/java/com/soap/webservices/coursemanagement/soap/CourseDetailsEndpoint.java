c6b2e7163af35e455c54f1017b4a8e29352b3b7c
Nick Mazzotta <nickamazzotta@gmail.com>
2018-02-16 17:51:22 -0500

    added simple delete course functionality

--- course-management/src/main/java/com/soap/webservices/coursemanagement/soap/CourseDetailsEndpoint.java
+++ course-management/src/main/java/com/soap/webservices/coursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,5 +1,7 @@
 package com.soap.webservices.coursemanagement.soap;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -7,6 +9,10 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.in28minutes.courses.CourseDetails;
+import com.in28minutes.courses.DeleteCourseDetailsRequest;
+import com.in28minutes.courses.DeleteCourseDetailsResponse;
+import com.in28minutes.courses.GetAllCourseDetailsRequest;
+import com.in28minutes.courses.GetAllCourseDetailsResponse;
 import com.in28minutes.courses.GetCourseDetailsRequest;
 import com.in28minutes.courses.GetCourseDetailsResponse;
 import com.soap.webservices.coursemanagement.soap.bean.Course;
@@ -21,17 +27,59 @@ public class CourseDetailsEndpoint {
 	@PayloadRoot(namespace="http://in28minutes.com/courses", localPart="GetCourseDetailsRequest")
 	@ResponsePayload
 	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
-		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
 		
 		Course course = service.findById(request.getId());
 		
-		CourseDetails courseDetails = new CourseDetails();
-		courseDetails.setId(request.getId());
-		courseDetails.setName("Microservices Course");
-		courseDetails.setDescription("An interesting course");
+		return mapCourseDetails(course);
+	}
+	
+	@PayloadRoot(namespace="http://in28minutes.com/courses", localPart="GetAllCourseDetailsRequest")
+	@ResponsePayload
+	public GetAllCourseDetailsResponse processAllCourseDetailsRequest(@RequestPayload GetAllCourseDetailsRequest request) {
 		
-		response.setCourseDetails(courseDetails);
+		List<Course> courses = service.findAll();
 		
+		return mapAllCourseDetails(courses);
+	}
+	
+	@PayloadRoot(namespace="http://in28minutes.com/courses", localPart="DeleteCourseDetailsRequest")
+	@ResponsePayload
+	public DeleteCourseDetailsResponse processDeleteCourseDetailsRequest(@RequestPayload DeleteCourseDetailsRequest request) {
+		
+		int status = service.deleteById(request.getId());
+		
+		return mapDelete(status);
+	}
+
+	private DeleteCourseDetailsResponse mapDelete(int status) {
+		DeleteCourseDetailsResponse response = new DeleteCourseDetailsResponse();
+		response.setStatus(status);
 		return response;
 	}
+
+	private GetCourseDetailsResponse mapCourseDetails(Course course) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		CourseDetails courseDetails = mapCourse(course);
+		response.setCourseDetails(courseDetails);
+		return response;
+	}
+
+	private CourseDetails mapCourse(Course course) {
+		CourseDetails courseDetails = new CourseDetails();
+		courseDetails.setId(course.getId());
+		courseDetails.setName(course.getName());
+		courseDetails.setDescription(course.getDescription());
+		return courseDetails;
+	}
+	
+	private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+		
+		GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+		for(Course course:courses) {
+			CourseDetails mapCourse = mapCourse(course);
+			response.getCourseDetails().add(mapCourse);
+		}
+		return response;
+	}
+
 }
