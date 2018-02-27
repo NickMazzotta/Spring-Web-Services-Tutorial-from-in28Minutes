b1ada7a7aea6221973ade828b86194e8784bed96
Nick Mazzotta <nickamazzotta@gmail.com>
2018-02-27 13:00:19 -0500

    added simple validations

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -3,12 +3,17 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 import com.in28minutes.rest.webservices.restfulwebservices.post.Post;
 
 public class User {
 	
 	private Integer userId;
+	@Size(min=2, message="Name should have at least 2 characters")
 	private String name;
+	@Past
 	private Date birthDate;
 	private List<Post> posts;
 	
